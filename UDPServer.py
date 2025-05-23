aaf743e624181225249c92a0d7c18d57801e4f0f
ButterCat_bmpDebian12 <62628392+Hans-Kerman@users.noreply.github.com>
2025-05-23 10:57:43 +0800

    now any mac address is available

--- UDPServer.py
+++ UDPServer.py
@@ -1,5 +1,21 @@
 #! /home/buttercat/codefield/socket_python/WakeOnLAN_Python/.venv/bin/python
+# -*- coding: UTF-8 -*-
 from socket import *
+import sys
+
+default_mac = "BCFCE7169D21"
+
+if len(sys.argv) > 2:
+    print("too many arguments!")
+    sys.exit(1)
+
+if (len(sys.argv) == 2) and str(sys.argv[1]) in ["-h","--help"]:
+    print ("输入目标12位mac地址")
+elif (len(sys.argv) == 2 ):
+    default_mac = "".join(char.upper() if char.isalpha() else char for char in sys.argv[1] if char.isalnum())
+    if len(default_mac) != 12:
+        print('请输入正确的mac地址')
+        sys.exit(1)
 
 # 255.255.255.255表示向任何网段发送广播消息
 address = ('255.255.255.255', 8)
@@ -10,8 +26,7 @@ s = socket(AF_INET, SOCK_DGRAM)
 # 设置socket属性
 s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
-default_mac = "BCFCE7169D21"
-
+source = default_mac
 default_mac = default_mac*16
 default_mac = "FFFFFFFFFFFF" + default_mac
 
@@ -20,7 +35,7 @@ message = bytes.fromhex(default_mac)
 # 发送广播消息
 s.sendto(message, address)
 
-print(' send ok !')
+print(' send ok ! '+str(source)+' is booted. ')
 
 # 关闭socket
 s.close()
